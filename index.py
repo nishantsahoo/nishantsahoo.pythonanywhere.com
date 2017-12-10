aaffade440160dc73e0b2578ce26656c553cb9a8
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-12-11 04:50:52 +0530

    Added index.html

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/', methods=["GET"])
-def hello():
-	return "Hi there! This is Nishant Sahoo"
+def my_page():
+	return send_from_directory("", 'index.html')
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 3333))
