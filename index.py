4a0a917349c0a21e00dbab2c21ea03ec9be7d528
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-12-11 05:14:42 +0530

    Update

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import simplejson as json
 import os
 from flask import Flask, request, send_from_directory
 
@@ -6,6 +7,10 @@ app = Flask(__name__, static_url_path='')
 @app.route('/', methods=["GET"])
 def my_page():
 	return send_from_directory("", 'index.html')
+
+@app.route('/', methods=["POST"])
+def my_page_result():
+	return json.dumps(request.form)
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 3333))
